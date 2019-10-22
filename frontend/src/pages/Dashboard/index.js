48e2d69dc3405a8bb2fc9256db0c717721752438
Heron Rodrigues <heronrs1@gmail.com>
2019-10-21 21:35:09 -0300

    Styling dashboard page

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import './style.css'
 
 export default function Dashboard() {
-    return <h1>asd</h1>;
+
+    const [spots, setSpots] = useState([]);
+
+    //Recebe uma função e um array de dependencias
+    //quando o valor no array for alterado, a função é executada
+    useEffect(() => {
+        async function loadSpots(){
+            const user_id = localStorage.getItem('user_id');
+            const response = await api.get('/dashboard',{
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+            console.log(response.data);
+        }
+
+        loadSpots();
+    }, []);
+
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map( spot =>(
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
+                    </li>
+                ))}
+            </ul>
+
+            <Link to='/new'>
+                <button className="btn">Cadastrar novo spot</button>
+            </Link>
+        </>
+    );
 }
