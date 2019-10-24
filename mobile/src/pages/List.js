029cb46ad82b8140ab6882da646b22de34999d71
Heron Rodrigues <heronrs1@gmail.com>
2019-10-24 17:07:05 -0300

    Creating mobile

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native'; 
+
+function List() {
+    return <View/>
+} 
+
+export default List();
---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native'; 
+
+function Login() {
+    return <View/>
+} 
+
+export default Login();
