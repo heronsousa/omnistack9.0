88acf7462e77223dfa6951cb05e1386521db0d7d
Heron Rodrigues <heronrs1@gmail.com>
2019-10-28 20:30:56 -0300

    Configuring real time update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,25 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+// Servidor acessar o protocolo web socket
+const socket = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+
+// Extrair servidor http do express
+const server = http.Server(app);
+
+// Server tem acesso ao protocolo web socket
+const io = socket(server);
+
+// Ouvir todos os usuários que entram na aplicação
+io.on('connection', socket => {
+    // socket é a conexão com o usuário
+    console.log('Usuário conectado', socket.id);
+});
 
 mongoose.connect('mongodb+srv://omnis:omnis@cluster0-hrfg8.mongodb.net/omnistack?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -17,4 +33,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
